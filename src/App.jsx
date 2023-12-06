b67b53cb946f9e8171a4ec3c916e19500684cae2
dorinengabdoh <ngabdohdorine@gmail.com>
2023-12-06 16:52:14 +0100

    doing some change

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import jsonData from './score.json'
 function App() {
 
 
-
   return (
     <>
     <div className="container">
-    <img src="https://t3.ftcdn.net/jpg/00/86/56/12/360_F_86561234_8HJdzg2iBlPap18K38mbyetKfdw1oNrm.jpg" alt="" />
+    <img src="https://images.unsplash.com/photo-1553095066-5014bc7b7f2d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2FsbCUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D" alt="" />
+    <div className="mainDiv">
+    {jsonData.map(item => (
+          <div key={item.clubs}>
+            {item.clubs.name}
+          </div>
+        ))}
     </div>
-    
-      
+    </div> 
     </>
   )
 }
